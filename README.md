47df354c3ca43505af24075f0cec8c9994c7d30b
simonjang <simon.jang.contact@gmail.com>
2017-04-27 18:09:29 +0200

    Added fix to documentation
    refactored module, fixed documentation
    cleared up documentation
    fixed documentation error
    
    
    added test, explained in options object in detail
    fixed typo in documentation
    Changed description in package.sjon
    add object argument and test

--- README.md
+++ README.md
@@ -13,28 +13,43 @@ $ npm install --save aws-sqs-push
 ## Usage
 
 ```js
-const sqspush = require('aws-sqs-push');
+const sqsPush = require('aws-sqs-push');
 
-sqspush('SomeMessage', 'QueueName', 'UserAWSAccountID');
+sqsPush('SomeMessage', 'QueueName', {}).then(messageId => {
+    console.log(messageId);
+    //=> '8a98f4d0-078b-5176-9af2-bbd871660ecb'
+});
 ```
 
 
 ## API
 
-### aws-sqs-push(message, queueName, AWSAccountID)
+### sqsPush(message, queueName, [options])
 
-#### input
+#### message
+
 
 Type: `string`
+
+Message that you want to push into queue.
 
 #### queueName
 
 Type: `string`
 
-#### AWSAccountID
+Name of the queue you want to push a message to.
+
+#### options
+the `options` object contains the AWS Account ID. Required unless provided in the environment variables of the lambda. See [AWS Lambda documentation](http://docs.aws.amazon.com/lambda/latest/dg/env_variables.html) for more information on how to configure the environment.
+
+Type: `object`
+
+##### options.awsAccountId
 
 Type: `string`
 
+ID of the root user of the AWS Account.
+
 ## License
 
-MIT © [Simon](https://github.com/SimonJang)
+MIT © [Simon Jang](https://github.com/SimonJang)
--- test.js
+++ test.js
@@ -1,16 +1,12 @@
 import test from 'ava';
 import sinon from 'sinon';
-import sqs from './fixtures/fake-sqs';
+import sqs from './test/fixtures/fake-sqs';
 import m from './';
 
 const sandbox = sinon.sandbox.create();
 
 test.before(() => {
-	const stubGetQueue = sandbox.stub(sqs, 'getQueueUrl');
-	stubGetQueue.withArgs({QueueName: 'demoQueue', QueueOwnerAWSAccountId: '123456789111'}).yields(undefined, {QueueUrl: 'https://sqs.eu-west-1.amazonaws.com/12345678912/somequeue'});
-	stubGetQueue.withArgs({QueueName: 'unknownQueue', QueueOwnerAWSAccountId: '123456789111'}).yields(undefined, {QueueUrl: ''});
-	const stubSendMessage = sandbox.stub(sqs, 'sendMessage');
-	stubSendMessage.withArgs({MessageBody: 'Test', QueueUrl: 'https://sqs.eu-west-1.amazonaws.com/12345678912/somequeue'}).yields(undefined, {MessageId: '123456789'});
+	sandbox.stub(process, 'env', {AWS_ACCOUNT_ID: '123456789111'});
 });
 
 test.after(() => {
@@ -26,18 +22,35 @@ test('error no queue name', async t => {
 });
 
 test('invalid queue name test', async t => {
-	t.throws(m('test', 'l[7777&&]l', '123456789111'), 'Invalid queue name');
+	t.throws(m('test', 'l[7777&&]l'), 'Invalid queue name');
 });
 
 test('invalid queueOwnerId test', async t => {
-	t.throws(m('test', 'demoQueue', '12345'), 'Invalid queueOwnerId');
+	sandbox.stub(process, 'env', {AWS_ACCOUNT_ID: '12'});
+	t.throws(m('test', 'demoQueue'), 'Invalid queueOwnerId');
 });
 
 test('invalid queue not found test', t => {
-	t.throws(m('test', 'unknownQueue', '123456789111'), 'Queue not found');
+	sandbox.stub(process, 'env', {AWS_ACCOUNT_ID: '123456789111'});
+	t.throws(m('test', 'unknownQueue'), 'Queue not found');
 });
 
-test('Should return MessageId value', async t => {
-	const check = await m('Test', 'demoQueue', '123456789111');
-	t.is(check.MessageId, '123456789');
+test('should return MessageId value', async t => {
+	const check = await m('Test', 'demoQueue');
+	t.is(check, '123456789');
+});
+
+test('should work without setting the enviroment variables', async t => {
+	sandbox.stub(process, 'env', {AWS_ACCOUNT_ID: ''});
+	const check = await m('Test', 'demoQueue', {awsAccountId: '123456789111'});
+	t.is(check, '123456789');
+});
+
+test('should stringify message when object', async t => {
+	sandbox.stub(process, 'env', {AWS_ACCOUNT_ID: '123456789111'});
+	await m({payload: 'something'}, 'demoQueue');
+	t.deepEqual(sqs.sendMessage.lastCall.args[0], {
+		MessageBody: JSON.stringify({payload: 'something'}),
+		QueueUrl: 'https://sqs.eu-west-1.amazonaws.com/12345678912/somequeue'
+	});
 });
