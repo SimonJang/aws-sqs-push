88468282ce66de3922175a7f9ae9a3d71c9a96be
simonjang <simon.jang.contact@gmail.com>
2017-04-27 18:17:22 +0200

    add object argument, test and changed documentation
    refactored module, fixed documentation
    cleared up documentation
    fixed documentation error
    
    
    added test, explained in options object in detail
    fixed typo in documentation
    Changed description in package.sjon
    add object argument and test
    
    
    Merge remote-tracking branch 'origin/master'
    add object argument, test and changed documentation

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 'use strict';
 const AWS = require('aws-sdk');
+const isObj = require('is-obj');
 const pify = require('pify');
+const awsIDCheck = require('is-aws-account-id');
 
 const sqs = new AWS.SQS();
 const request = pify(sqs.sendMessage.bind(sqs));
 const getQueueUrl = pify(sqs.getQueueUrl.bind(sqs));
 
-module.exports = (message, queueName, queueOwnerId) => {
+module.exports = (message, queueName, options) => {
+	options = Object.assign({
+		awsAccountId: process.env.AWS_ACCOUNT_ID
+	}, options);
+
 	if (!message) {
 		return Promise.reject(new TypeError('Please provide a message'));
 	}
@@ -16,26 +22,28 @@ module.exports = (message, queueName, queueOwnerId) => {
 	if (queueName.length > 80 || !/^[a-zA-Z0-9_-]{1,80}$/i.test(queueName)) {
 		return Promise.reject(new TypeError('Invalid queue name'));
 	}
-	if (!queueOwnerId || queueOwnerId.length !== 12 || !/([0-9]{12})+/i.test(queueOwnerId)) {
+	if (!options.awsAccountId || options.awsAccountId.length !== 12 || !awsIDCheck(options.awsAccountId)) {
 		return Promise.reject(new TypeError('Invalid queueOwnerId'));
 	}
+
+	message = isObj(message) ? JSON.stringify(message) : message;
 
 	return getQueueUrl(
 		{
 			QueueName: queueName,
-			QueueOwnerAWSAccountId: queueOwnerId
+			QueueOwnerAWSAccountId: options.awsAccountId
 		}
 	).then(url => {
-		if (url.QueueUrl && url.QueueUrl.length !== 0) {
-			return url.QueueUrl;
+		if (!url.QueueUrl && url.QueueUrl.length === 0) {
+			throw new TypeError('Queue not found');
 		}
-		return Promise.reject(new TypeError('Queue not found'));
-	})
-		.then(url => {
-			return {
-				MessageBody: message,
-				QueueUrl: url
-			};
-		})
-		.then(params => request(params));
+		return url.QueueUrl;
+	}).then(url => {
+		return {
+			MessageBody: message,
+			QueueUrl: url
+		};
+	}).then(
+		params => request(params)
+	).then(result => result.MessageId);
 };
